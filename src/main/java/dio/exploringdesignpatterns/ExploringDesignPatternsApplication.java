ab0a47f90055697d0bd50473a6acd1c0afb08f4a
Samuel Vinícius Basílio <samuelvinib@gmail.com>
2024-04-10 12:50:56 -0300

    first commit

---
+++ src/main/java/dio/exploringdesignpatterns/ExploringDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package dio.exploringdesignpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploringDesignPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExploringDesignPatternsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dio/exploringdesignpatterns/ExploringDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.exploringdesignpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploringDesignPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
